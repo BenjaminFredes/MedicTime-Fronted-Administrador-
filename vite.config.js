57f00d9e48973e9cfd56e8bc4253577752de4dd0
Ma-Duoc <ma.avelloc@duocuc.cl>
2026-09-09 21:33:45 -0300

    fix: corrige base path para MedicTime-Fronted-Administrador-

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MedicTime-Dashboard/',
+  base: '/MedicTime-Fronted-Administrador-/',
   server: {
     port: 3000,
     open: true
